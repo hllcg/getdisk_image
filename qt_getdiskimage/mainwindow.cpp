408bf6f7e138168a8e0510b15a1597abf65568c6
HamdiAlay <hlleee@hotmail.com>
2013-09-10 09:21:05 +0300

    asd

--- qt_getdiskimage/mainwindow.cpp
+++ qt_getdiskimage/mainwindow.cpp
@@ -3,14 +3,20 @@
 #include <string>
 #include <iostream>
 #include <stdio.h>
+#include <QFileDialog>
+#include <QDateTime>
+QString filebrowse;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->pushButton->setText("Get Devices");
+    ui->textEdit->hide();
     //connect(button, SIGNAL(clicked()), this, SLOT(buttonPressed()));
     connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(doldur()));
+    connect(ui->pushButton_2,SIGNAL(clicked()),this,SLOT(getfiledialog()));
 }
 
 MainWindow::~MainWindow()
@@ -22,10 +28,17 @@ void MainWindow::inspect(void)
 {
     unsigned char device[100][50];
     return;
-
-
 }
-
+void MainWindow::getfiledialog()
+{
+    QFileDialog * fd = new QFileDialog;
+    filebrowse = fd->getOpenFileName(this);
+    filebrowse +="/image_"+ QDate::currentDate().toString("dd.MM.yyyy_hh.mm");
+    //ui->comboBox->addItem(filebrowse);
+    //QFile * file = new QFile; Here error starts...
+    //txt->setPlainText(file->read(fd->FileName)); this doesn't work.
+    ui->label_savepath->setText(filebrowse);
+}
 
 
 QString exec_cmd(char* cmd)
@@ -45,6 +58,19 @@ QString exec_cmd(char* cmd)
 
 void MainWindow::doldur()
 {
-    QString str = exec_cmd("ls -l /dev/sda512");
-    ui->textEdit->setText(str);
+    QString str = exec_cmd("ls -l /dev/sd*");
+    QStringList pieces = str.split( "\n", QString::SkipEmptyParts);
+    QString item,str2;
+    for(int i=0;i<pieces.length();i++)
+    {
+        item = pieces.value(i);
+        item.remove(0,42);
+        ui->comboBox->addItem(item);
+        str2 +=item+"\n";
+
+    }
+    ui->textEdit->setText(str2);
+    //ui->comboBox->addItem("halil");
+    //ui->comboBox->addItem("caglar");
+    //ui->comboBox->addItem("UEKAE");
 }
--- qt_getdiskimage/mainwindow.h
+++ qt_getdiskimage/mainwindow.h
@@ -21,6 +21,7 @@ private:
 
 public slots:
     void doldur();
+    void getfiledialog();
 
 };
 
